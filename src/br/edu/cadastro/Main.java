a0f979c1c2cef283f2ae7da8fab6c5861c521d65
EduardoFilho777 <duca96_@hotmail.com>
2025-11-28 19:20:09 -0300

    Primeiro commit - Projeto Cadastro de Alunos em Java

---
+++ src/br/edu/cadastro/Main.java
@@ -0,0 +1,26 @@
+package br.edu.cadastro;
+
+import br.edu.cadastro.exception.CadastroException;
+import br.edu.cadastro.model.Aluno;
+import br.edu.cadastro.service.CadastroService;
+
+public class Main {public static void main(String[] args) {
+        CadastroService service = new CadastroService();
+
+        try {
+            Aluno aluno1 = new Aluno("Eduardo", 20, "2025001");
+            service.cadastrarAluno(aluno1);
+
+            Aluno aluno2 = new Aluno(null, 22, "2025002");
+            service.cadastrarAluno(aluno2); // Vai lançar exceção
+
+        } catch (CadastroException e) {
+            System.err.println("Erro: " + e.getMessage());
+        }
+
+        service.listarAlunos();
+    }
+}
+
+    
+
